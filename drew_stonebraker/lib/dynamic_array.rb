deed2174d39b7e15c1698bbc242d0946be6f9e79
drstonebraker <drew.stonebraker@gmail.com>
2017-10-17 12:46:20 -0700

    Change time complexity of check_index

--- drew_stonebraker/lib/dynamic_array.rb
+++ drew_stonebraker/lib/dynamic_array.rb
@@ -69,7 +69,7 @@ class DynamicArray
   attr_writer :length
 
   def check_index(index)
-    raise 'index out of bounds' unless (0...@length).include?(index)
+    raise 'index out of bounds' unless index.between?(0, @length)
   end
 
   def check_capacity!
