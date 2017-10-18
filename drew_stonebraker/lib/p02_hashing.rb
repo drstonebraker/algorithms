35b667cbae818e9cca3c9948c40858608fa42506
drstonebraker <drew.stonebraker@gmail.com>
2017-10-17 18:37:27 -0700

    Finish hashing spec

--- drew_stonebraker/lib/p02_hashing.rb
+++ drew_stonebraker/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    mapped = self.map do |el|
+      if el.is_a?(Hash)
+        el.hash
+      else
+        el
+      end
+    end
+
+    'Array'.concat(mapped.inspect).hash
   end
 end
 
 class String
   def hash
+    mapped = ('String'.concat(self)).chars.map.with_index do |ch, idx|
+      (idx.to_s.concat(ch.ord.to_s)).to_i.hash
+    end
+    mapped.inject(:^)
   end
 end
 
@@ -16,6 +29,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    'Hash'.concat(self.to_a.sort.hash.to_s).hash
   end
 end
