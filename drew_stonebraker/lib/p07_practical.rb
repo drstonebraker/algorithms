ec25ef3e24e7c2de1797bffcef522846835d8fa9
drstonebraker <drew.stonebraker@gmail.com>
2017-10-17 20:12:50 -0700

    Pass all specs

--- drew_stonebraker/lib/p07_practical.rb
+++ drew_stonebraker/lib/p07_practical.rb
@@ -1,5 +1,20 @@
 require_relative 'p05_hash_map'
 
-def can_string_be_palindrome?(string) 
-  
+def can_string_be_palindrome?(string)
+  counts = HashMap.new
+
+  string.each_char do |ch|
+    counts[ch] = counts[ch] || 0
+    counts[ch] += 1
+  end
+
+  ones_count = 0
+
+  counts.each do |k, v|
+    return false if v > 2
+    ones_count += 1 if v == 1
+    return false if ones_count > 1
+  end
+
+  true
 end
