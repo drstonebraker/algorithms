302db02d6b3bb8dfab144f6c3430606533f63f5d
drstonebraker <drew.stonebraker@gmail.com>
2017-10-17 18:07:20 -0700

    Finish IntSet

--- drew_stonebraker/lib/p01_int_set.rb
+++ drew_stonebraker/lib/p01_int_set.rb
@@ -1,22 +1,32 @@
 class MaxIntSet
   def initialize(max)
+    @max = max
+    @store = Array.new(max)
   end
 
   def insert(num)
+    validate!(num)
+    @store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    @store[num] = false
   end
 
   def include?(num)
+    validate!(num)
+    @store[num]
   end
 
   private
 
   def is_valid?(num)
+    num.between?(0, @max)
   end
 
   def validate!(num)
+    raise 'Out of bounds' unless is_valid?(num)
   end
 end
 
@@ -27,18 +37,24 @@ class IntSet
   end
 
   def insert(num)
+    bucket = self[num]
+    bucket << num unless bucket.include?(num)
   end
 
   def remove(num)
+    bucket = self[num]
+    bucket.delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
