e4e7041f8f880487bc2c634c80393a3988417335
drstonebraker <drew.stonebraker@gmail.com>
2017-10-22 18:11:49 -0700

    Pass index specs

--- heaps/lib/heap.rb
+++ heaps/lib/heap.rb
@@ -2,6 +2,7 @@ class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @prc = prc || Proc.new {|a, b| a <=> b}
   end
 
   def count
@@ -18,9 +19,13 @@ class BinaryMinHeap
 
   public
   def self.child_indices(len, parent_index)
+    double = parent_index * 2
+    [double.next, (double + 2)].reject {|idx| idx >= len}
   end
 
   def self.parent_index(child_index)
+    raise 'root has no parent' if child_index.zero?
+    child_index.pred / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
