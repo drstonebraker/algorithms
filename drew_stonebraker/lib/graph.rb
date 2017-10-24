cef3d2b1f560a8a8339fc09c9901eda4d75f3d6e
drstonebraker <drew.stonebraker@gmail.com>
2017-10-24 10:56:46 -0700

    Write graph classes

--- drew_stonebraker/lib/graph.rb
+++ drew_stonebraker/lib/graph.rb
@@ -1,15 +1,27 @@
 class Vertex
-  def initialize(value)
+  attr_reader :value
+  attr_accessor :in_edges, :out_edges
 
+  def initialize(value)
+    @value = value
+    @in_edges = Set.new
+    @out_edges = Set.new
   end
 end
 
 class Edge
   def initialize(from_vertex, to_vertex, cost = 1)
-
+    from_vertex.out_edges.add(self)
+    to_vertex.in_edges.add(self)
+    @from = from_vertex
+    @to = to_vertex
+    @cost = cost
   end
 
   def destroy!
-
+    @from.out_edges.remove(self)
+    @to.in_edges.remove(self)
+    @from = nil
+    @to = nil
   end
 end
