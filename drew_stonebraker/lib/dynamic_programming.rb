af668213a3f6c13af9146463778bebd40f960627
drstonebraker <drew.stonebraker@gmail.com>
2017-10-24 21:20:27 -0700

    Pass blair nums

--- drew_stonebraker/lib/dynamic_programming.rb
+++ drew_stonebraker/lib/dynamic_programming.rb
@@ -1,11 +1,17 @@
 class DynamicProgramming
 
   def initialize
-
+    @blair_nums = {1 => 1, 2 => 2}
   end
 
   def blair_nums(n)
+    return @blair_nums[n] if @blair_nums[n]
+    result = blair_nums(n - 1) + blair_nums(n - 2) + blair_odd(n)
+    @blair_nums[n] = result
+  end
 
+  def blair_odd(k)
+    (k - 1) * 2 - 1
   end
 
   def frog_hops_bottom_up(n)
