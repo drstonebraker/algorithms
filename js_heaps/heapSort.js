4ede09ad73e85f8ddbb3e70a9faf6b60c36c80c4
drstonebraker <drew.stonebraker@gmail.com>
2017-12-02 20:39:37 -0800

    Pass spec for sorting unsorted array

--- js_heaps/heapSort.js
+++ js_heaps/heapSort.js
@@ -1,15 +1,18 @@
-const BinaryMinHeap = require('heap')
+const BinaryMinHeap = require('./heap')
 
-Array.prototype.heapSort = function heapSort(compareFn) {
+const heapSort = function heapSort(compareFn) {
     const bmh = new BinaryMinHeap(compareFn)
 
     this.forEach((el) => bmh.push(el))
 
     const result = []
+    const length = bmh.count()
 
-    for (let i = 0; i < bmh.count; i++) {
+    for (let i = 0; i < length; i++) {
         result.push(bmh.extract())
     }
 
     return result
 }
+
+module.exports = heapSort
---
+++ js_heaps/heapSort.test.js
@@ -0,0 +1,11 @@
+Array.prototype.heapSort = require('./heapSort')
+
+describe('Array', () => {
+    describe('heapSort', () => {
+        test('sorts an unsorted array', () => {
+            let arr = [4,2,1,3,5,7,8,9]
+            arr = arr.heapSort()
+            expect(arr).toEqual([1,2,3,4,5,7,8,9])
+        })
+    })
+})
