2a2418bf9ba9b02b0c9e7cb2a6ec09809949859b
drstonebraker <drew.stonebraker@gmail.com>
2017-12-03 16:20:35 -0800

    Write and pass reversed array spec for heapSort

--- js_heaps/heapSort.test.js
+++ js_heaps/heapSort.test.js
@@ -1,11 +1,17 @@
 Array.prototype.heapSort = require('./heapSort')
 
 describe('Array', () => {
-    describe('heapSort', () => {
-        test('sorts an unsorted array', () => {
-            let arr = [4,2,1,3,5,7,8,9]
-            arr = arr.heapSort()
-            expect(arr).toEqual([1,2,3,4,5,7,8,9])
-        })
+  describe('heapSort', () => {
+    test('sorts an unsorted array', () => {
+      let arr = [4,2,1,3,5,7,8,9]
+      arr = arr.heapSort()
+      expect(arr).toEqual([1,2,3,4,5,7,8,9])
     })
+
+    test('sorts a reversed array', () => {
+      let arr = [5,4,3,2,1]
+      arr = arr.heapSort()
+      expect(arr).toEqual([1,2,3,4,5])
+    })
+  })
 })
